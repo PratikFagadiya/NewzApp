c6f98e23020b08c74d32ee6e1781cc78c178d963
Pratik Fagadiya <pratikfagadiya@gmail.com>
2021-07-18 20:52:07 +0530

    chnage bottom navigationbar labelVisibilityMode

--- app/src/main/java/com/newsapp/withmvvm/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/newsapp/withmvvm/ui/fragments/ArticleFragment.kt
@@ -1,6 +1,6 @@
 package com.newsapp.withmvvm.ui.fragments
 
-import android.os.Bundle
+    import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -28,7 +28,7 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
 
         webView.apply {
             webViewClient = WebViewClient()
-            loadUrl(article.url)
+            article.url?.let { loadUrl(it) }
         }
 
         fab.setOnClickListener {
